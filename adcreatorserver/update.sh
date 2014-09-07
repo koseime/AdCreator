d6697864d04c386dd38b47b2385dcae82c35b39e
lance@kosei.me <lance@kosei.me>
2014-09-07 14:29:25 -0700

    more images

--- adcreatorserver/update.sh
+++ adcreatorserver/update.sh
@@ -1,1 +1,2 @@
- cd ../native;make clean;make;cd ../adcreatorserver; make clean;make;./ad-preview-creator-server
+pkill -9 -f preview 
+cd ../native;make clean;make;cd ../adcreatorserver; make clean;make;./ad-preview-creator-server
