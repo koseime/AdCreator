1f13b2e1da8a6714619aaa165f641a71a3ef9196
Sam Meder <sam@kosei.me>
2014-09-18 20:31:40 -0700

    Add build script

---
+++ native/jenkins/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+set -e
+
+WORKSPACE=$1
+BUILD_NUMBER=$2
+
+cd native 
+HADOOP_SRC=/usr/src/hadoop make clean all 
+mvn -f jenkins/repository/pom.xml -X deploy:deploy-file --settings jenkins/repository/settings.xml -Durl=dav:https://repository-kosei.forge.cloudbees.com/release -DrepositoryId=kosei.release -Dfile=${WORKSPACE}/native/ad-creator -DgroupId=com.kosei.jobs -DartifactId=ad-creator -Dversion=0.${BUILD_NUMBER} -Dpackaging=binary
