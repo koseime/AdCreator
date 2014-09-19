a31952503a36c0f7fa9f25b20c460d7b79f6c340
Sam Meder <sam@kosei.me>
2014-09-18 20:47:33 -0700

    Turn of mvn debug output

--- native/jenkins/build.sh
+++ native/jenkins/build.sh
@@ -8,4 +8,4 @@ BUILD_NUMBER=$2
 
 cd native 
 HADOOP_SRC=/usr/src/hadoop make clean all 
-mvn -f jenkins/repository/pom.xml -X deploy:deploy-file --settings jenkins/repository/settings.xml -Durl=dav:https://repository-kosei.forge.cloudbees.com/release -DrepositoryId=kosei.release -Dfile=${WORKSPACE}/native/ad-creator -DgroupId=com.kosei.jobs -DartifactId=ad-creator -Dversion=0.${BUILD_NUMBER} -Dpackaging=binary
+mvn -f jenkins/repository/pom.xml deploy:deploy-file --settings jenkins/repository/settings.xml -Durl=dav:https://repository-kosei.forge.cloudbees.com/release -DrepositoryId=kosei.release -Dfile=${WORKSPACE}/native/ad-creator -DgroupId=com.kosei.jobs -DartifactId=ad-creator -Dversion=0.${BUILD_NUMBER} -Dpackaging=binary
