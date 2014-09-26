b04786e8a29b30e9966d37ea22a1b6e4729365ff
chantat <c.eksombatchai@gmail.com>
2014-09-26 14:23:00 -0700

    forgot to add the actual test file to commit

---
+++ native/test/ImageMagickLayoutEngineTest.cpp
@@ -0,0 +1,39 @@
+/*
+ * AdCreatorTest.cpp
+ *
+ *  Created on: Sep 26, 2014
+ *      Author: chantat
+ */
+
+#define BOOST_TEST_MAIN
+
+#include "../ImageMagickLayoutEngine.h"
+
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include <boost/test/unit_test.hpp>
+
+using namespace std;
+
+BOOST_AUTO_TEST_CASE( createTest )
+{
+	const string image_file_path = "test/resources/Space-Rocket-Emoji.png";
+	const string title = "Foo Foo Foo";
+	const string copy = "Bar Bar Bar";
+
+	ifstream fs(image_file_path.c_str());
+	const string productBlob((istreambuf_iterator<char>(fs)), istreambuf_iterator<char>());
+
+	ImageMagickLayoutEngine engine;
+	engine.importResources("test/resources/layout_resource.tar.gz");
+
+	vector<pair<string, string> > generatedAds;
+	engine.createAllLayouts(productBlob, title, copy, &generatedAds);
+
+	BOOST_CHECK_EQUAL(generatedAds.size(), 1);
+	BOOST_CHECK(generatedAds[0].second.size() > 0);
+}
+
+
