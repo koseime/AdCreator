20e2db6d3256a1286f0d7dac77b68ad7d3b9c29e
Sam Meder <sam@kosei.me>
2014-09-29 20:36:59 -0700

    Add check target to standard build script

--- native/jenkins/build.sh
+++ native/jenkins/build.sh
@@ -7,6 +7,6 @@ WORKSPACE=$1
 BUILD_NUMBER=$2
 
 cd native 
-HADOOP_SRC=/usr/src/hadoop make clean all 
+HADOOP_SRC=/usr/src/hadoop make clean all check
 mvn -f jenkins/repository/pom.xml deploy:deploy-file --settings jenkins/repository/settings.xml -Durl=dav:https://repository-kosei.forge.cloudbees.com/release -DrepositoryId=kosei.release -Dfile=${WORKSPACE}/native/ad-creator -DgroupId=com.kosei.jobs -DartifactId=ad-creator -Dversion=0.${BUILD_NUMBER} -Dpackaging=binary
 mvn -f jenkins/repository/pom.xml deploy:deploy-file --settings jenkins/repository/settings.xml -Durl=dav:https://repository-kosei.forge.cloudbees.com/release -DrepositoryId=kosei.release -Dfile=${WORKSPACE}/native/ad-creator-client -DgroupId=com.kosei.jobs -DartifactId=ad-creator-client -Dversion=0.${BUILD_NUMBER} -Dpackaging=binary
